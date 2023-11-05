c13208fcb4d3b37d2c5ff2355acb67056c322a38
Griego86 <dimi1086@hotmail.com>
2023-11-05 11:27:03 -0800

    added function to display username and logout button

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
-// Middleware for parsing URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,79 +22,40 @@ function generateRandomString() {
   return randomString;
 }
 
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username // Pass the username to the template
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies.username // Pass the username to the template
+  };
+  res.render("urls_new", templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id]; // Retrieving the long URL associated with the ID from urlDatabase
-  const templateVars = { id, longURL };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL; // Assuming the form field name is "longURL"
-  const id = generateRandomString(); // Generate a random ID
-  urlDatabase[id] = longURL; // Save the new ID-longURL pair to the urlDatabase
-  res.redirect(`/urls/${id}`); // Redirect to the new URL page for the newly generated short URL
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id;
-  if (urlDatabase[id]) {
-    delete urlDatabase[id]; // Using the delete operator to remove the URL resource
-    res.redirect("/urls"); // Redirect back to the urls_index page
-  } else {
-    res.status(404).send("URL not found");
-  }
-});
-
-app.get("/u/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  if (longURL) {
-    res.redirect(longURL);
-  } else {
-    res.status(404).send("Short URL not found");
-  }
-});
-
-app.post("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const newURL = req.body.newLongURL; // Assuming the form field name is "newLongURL"
-
-  if (urlDatabase[id]) {
-    urlDatabase[id] = newURL; // Update the stored long URL with the new value
-    res.redirect("/urls"); // Redirect back to the urls_index page
-  } else {
-    res.status(404).send("URL not found");
-  }
-});
+// Other routes...
 
 app.post("/login", (req, res) => {
   const { username } = req.body;
   res.cookie('username', username); // Set the cookie with the username
   res.redirect('/urls'); // Redirect the browser back to the /urls page
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username'); // Clear the username cookie
+  res.redirect('/urls'); // Redirect the browser back to the /urls page
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`);
 });
