2c95f61fa501bac4ab3ffc54193c1f21293cec2b
Griego86 <dimi1086@hotmail.com>
2023-11-04 18:48:35 -0700

    Added a second route and template

--- express_server.js
+++ express_server.js
@@ -26,6 +26,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id]; // Retrieving the long URL associated with the ID from urlDatabase
+  const templateVars = { id, longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
